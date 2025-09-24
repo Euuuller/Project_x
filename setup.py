2de10cddad9e7dbc62bd036ac95631ad79cfbce8
Euuuller <euller.santos.duarte@gmail.com>
2025-09-24 06:24:35 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Esse projeto será feito para meu template de portfólio!',
+    author='Euller dos Santos Rodrigues Duarte',
+    license='MIT',
+)
